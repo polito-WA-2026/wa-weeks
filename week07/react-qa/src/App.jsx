36823d181fc8a6717e3a43fa78d07da904215bb7
Enrico Masala <enrico.masala@polito.it>
2026-04-13 16:41:01 +0200

    Finished example

--- week07/react-qa/src/App.jsx
+++ week07/react-qa/src/App.jsx
@@ -1,77 +1,115 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { Table, Button } from 'react-bootstrap';
-import './App.css'
-import { Answer, Question } from './QAModels';
+import { Col, Container, Row, Button, Table, Navbar } from 'react-bootstrap';
+import './App.css';
+
+import { Question } from './QAModels.js';
+
+const question = new Question(1, 'Best way of enumerating an array in JS?', 'Enrico', '2024-03-01');
+question.init();
+const answerList = question.getAnswers();
+
 
 function MyHeader(props) {
   return (
-     <header>
-        <nav className="navbar bg-body-tertiary">
-          <div className="container-fluid">
-            <div>
-              <i className="bi bi-lightbulb" style={{fontSize: "2rem", color: "cornflowerblue"}}></i>
-              <a className="navbar-brand" href="#">
-                {props.title || "React Q&A"}
-              </a>
-            </div>
-          </div>
-        </nav>
-      </header>
-  );
+		<Navbar bg="primary" variant="dark">
+      <Navbar.Brand className="mx-2">
+      <i className="bi bi-collection-play" />
+      {/* props.appName just in case you want to set a different app name */}
+			{props.appName || "HeapOverrun"}
+      </Navbar.Brand>
+		</Navbar>
+	);
+}
+
+
+function MyFooter(props) {
+  return (<footer>
+    <p>&copy; Web Applications</p>
+    <div id="time"></div>
+  </footer>);
 }
 
 function AnswerRow(props) {
-  const ans = props.answer;
-  return (  <tr>
-    <td>{ans.date.format("YYYY-MM-DD")}</td>
-    <td>{ans.text}</td>
-    <td>{ans.respondent}</td>
-    <td>{ans.score}</td>
-    <td><Button>Vote</Button></td>
-  </tr>);
+  const e = props.answer;
+  return (
+    <tr>
+      <td>{e.date.format("YYYY-MM-DD")}</td>
+      <td>{e.text}</td>
+      <td>{e.respondent}</td>
+      <td>{e.score}</td>
+      <td><Button variant="primary">Vote</Button></td>
+    </tr>
+  );
 }
+
+function MyTable(props) {
+  return (
+    <Table>
+      {/* <Table striped bordered hover> */}
+    <thead>
+      <tr>
+        <th>Date</th>
+        <th>Text</th>
+        <th>Author</th>
+        <th>Score</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+      <tbody>        
+        {/* the key can also be the answer id, if unique */}
+				{props.listOfAnswers.map( (e) => 
+				 <AnswerRow key={e.id} answer={e} /> )
+        }
+    </tbody>
+    </Table>
+  )
+}
+
+function Main(props) {
+  return (<>
+    <Row>
+      <Col xs={9}>
+        <p className="question">Best way of enumerating an array in JS?</p>
+      </Col>
+      <Col xs={3}>
+        <p className="question">Author: Enrico</p>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <h2>Current Answers</h2>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <MyTable listOfAnswers={answerList} />
+      </Col>
+    </Row>
+  </>
+  );
+}
+
 
 
 function App() {
-  const question = new Question(1, 'Best way of enumerating an array in JS?', 'Enrico', '2024-03-01');
-  question.init();
-  const answers = question.getAnswers();
-
-  console.log(answers);
 
   return (
-    <div className="container-fluid">
-      <MyHeader title="HeapOverrun" />
-      <main>
-        <div className="row lead mt-4">
-          <div className="d-flex justify-content-center">
-            <p className="question" id="questionText">Best way of enumerating an array in JS?</p>
-            <p className="question ms-5">by <span id="questionAuthorName">Enrico Masala</span></p>
-          </div>
-        </div>
-        <h2>Answers</h2>
-        <Table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Text</th>
-              <th>Author</th>
-              <th>Score</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody id="answers">
-            {answers.map(ans => <AnswerRow key={ans.id} answer={ans} />)}
-          </tbody>
-        </Table>
-      </main>
-      <footer>
-        &copy; Web Applications  <span id="time"></span>
-      </footer>
-    </div>
-
+    <Container fluid>
+      <Row>
+        <Col>
+          <MyHeader />
+        </Col>
+      </Row>
+      <Main />
+      <Row>
+        <Col>
+          <MyFooter />
+        </Col>
+      </Row>
+    </Container>
   )
 }
+
 
 export default App
