f9510b6d96e4359cb92ca2d408f8ea85d291e9c4
Enrico Masala <enrico.masala@polito.it>
2026-04-16 10:20:53 +0200

    Testing Count from slides

--- week07/my-app/src/App.jsx
+++ week07/my-app/src/App.jsx
@@ -1,14 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 import MyButton from './Button';
 
 function App() {
 	return (
 		<p>
 		Press here: <MyButton language='en' />
+		<Counter initialCount={0} />
 		</p>
 	);
 }
 
+function Counter(props) {
+	const [count, setCount] = useState(props.initialCount);
+	return (
+		<>
+			Count: {count}
+			<Button onClick={() => setCount(props.initialCount)}>Reset</Button>
+			<button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+			<button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+		</>
+	);
+}
 
 export default App
