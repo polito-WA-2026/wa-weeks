18caf4adf337879b3d149250c4f4e0b6d39685e1
Enrico Masala <enrico.masala@polito.it>
2026-04-16 10:37:00 +0200

    ButtonGroup

--- week07/my-app/src/App.jsx
+++ week07/my-app/src/App.jsx
@@ -1,14 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import MyButton from './Button';
 
 function App() {
 	return (
-		<p>
-		Press here: <MyButton language='en' />
-		<Counter initialCount={0} />
-		</p>
+		<>
+			<p>
+				Press here: <MyButton language='en' />
+				<Counter initialCount={0} />
+			</p>
+			<ButtonGroup names={['Chess', 'Poker', 'Black Jack', 'Go']} />
+		</>
 	);
 }
 
@@ -23,5 +26,32 @@ function Counter(props) {
 		</>
 	);
 }
+
+
+function SimpleButton (props) {
+	return (
+		<Button variant={props.selected?'primary':'secondary'}
+		   onClick={() => props.choose(props.index)} >{props.name}</Button>
+	)
+}
+
+function ButtonGroup (props) {
+	const [selected, setSelected] = useState(3);
+
+	const chooseButton = (index) => setSelected(index);
+	return (
+		<Form>
+		{
+			props.names.map((e,idx) => <SimpleButton
+			  name={e} index={idx} key={idx}
+			  selected={idx===selected} choose={chooseButton}></SimpleButton>)
+		}
+		</Form>
+	)
+}
+
+
+{/* <ButtonGroup names={['Chess', 'Poker', 'Black Jack', 'Go']} /> */}
+
 
 export default App
